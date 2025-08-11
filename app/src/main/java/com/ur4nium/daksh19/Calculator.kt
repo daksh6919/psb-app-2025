28d4b4666c0bb38f258e403e6bca3e3fbf8a8ddf
Lucifer <rupees3669@gmail.com>
2025-08-11 22:46:24 +0530

    Initial commit

--- app/src/main/java/com/ur4nium/daksh19/Calculator.kt
+++ app/src/main/java/com/ur4nium/daksh19/Calculator.kt
@@ -2,9 +2,14 @@ package com.ur4nium.daksh19
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ur4nium.daksh19.databinding.ActivityCalculatorBinding
 
 class Calculator : AppCompatActivity() {
@@ -15,6 +20,34 @@ class Calculator : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // imageslider
+
+        val imageList = ArrayList<SlideModel>()
+        val db = Firebase.firestore
+
+        db.collection("calculator")
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    Log.d("Firestore", "No images found in the collection.")
+                    return@addOnSuccessListener
+                }
+                // Loop through documents and add URLs to the list
+                for (document in documents) {
+                    val imageUrl = document.getString("url")
+                    if (imageUrl != null) {
+                        imageList.add(SlideModel(imageUrl, ScaleTypes.CENTER_CROP))
+                    }
+                }
+                // Set the fetched images to the slider
+                binding.imageSlider.setImageList(imageList)
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Firestore", "Error getting documents: ", exception)
+            }
+
+                 //image slider code ends here
 
         // Back button
         binding.backButton.setOnClickListener { finish() 
