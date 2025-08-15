399bef834ba7071d45750212561c7bdbddfb4fd8
DAKSH <daksh.14.wadhewa@gmail.com>
2025-08-15 14:56:24 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/ur4nium/daksh19/url_spam.kt

---
+++ app/src/main/java/com/ur4nium/daksh19/videocyber.kt
@@ -0,0 +1,17 @@
+package com.ur4nium.daksh19
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VideoCyber : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_videocyber)
+
+    }
+
+}
